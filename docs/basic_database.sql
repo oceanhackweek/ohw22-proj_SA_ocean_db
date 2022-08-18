0a492af6cf3d26c8e6cd533da704a7dfb620f6a1
Danilo A. Silva <nilodna@gmail>
2022-08-18 19:12:28 -0300

    changing the database structure to insert a new column in the metadata table, for the filename

--- docs/basic_database.sql
+++ docs/basic_database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE pnboia_buoy_metadata (
+CREATE TABLE pnboia_buoy_metadata2 (
     pnboia_id INT,
     location VARCHAR (255) UNIQUE NOT NULL,
     latitude float NOT NULL,
@@ -6,6 +6,7 @@ CREATE TABLE pnboia_buoy_metadata (
     start_date DATE NOT NULL,
     final_date DATE,
     status INT NOT NULL,
+    fname VARCHAR,
     PRIMARY KEY(pnboia_id)
 );
 
@@ -34,10 +35,10 @@ CREATE TABLE pnboia (
     wvdir_flag_qartod INT
 );
 
-INSERT INTO pnboia_buoy_metadata VALUES (27, 'Alcatrazes', -24.129017, -45.676933, '2022-04-07', NULL, 1);
+INSERT INTO pnboia_buoy_metadata VALUES (27, 'Alcatrazes', -24.129017, -45.676933, '2022-04-07', NULL, 1, 'alcatrazes');
 
-INSERT INTO pnboia_buoy_metadata VALUES (20, 'Abrolhos', -17.98415, -38.717933, '2022-01-04', NULL, 1);
+INSERT INTO pnboia_buoy_metadata VALUES (20, 'Abrolhos', -17.98415, -38.717933, '2022-01-04', NULL, 1, 'abrolhos');
 
-INSERT INTO pnboia_buoy_metadata VALUES (28, 'Fernando de Noronha', -3.7981, -32.3717, '2022-06-04', NULL, 1);
+INSERT INTO pnboia_buoy_metadata VALUES (28, 'Fernando de Noronha', -3.7981, -32.3717, '2022-06-04', NULL, 1, 'noronha');
 
-INSERT INTO pnboia_buoy_metadata VALUES (29, 'Imbituba', -28.35045, -48.649833, '2022-07-26', NULL, 1);
+INSERT INTO pnboia_buoy_metadata VALUES (29, 'Imbituba', -28.35045, -48.649833, '2022-07-26', NULL, 1, 'imbituba');
